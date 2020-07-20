c0ab3c8dc9cc03f07dcc311eda5cd81b28ef9f61
perwestling <per.o.westling@gmail.com>
2020-07-20 10:44:57 +0200

    Disable route_bonus ability as it seems to be broken

--- lib/engine/round/g_18_al/operating.rb
+++ lib/engine/round/g_18_al/operating.rb
@@ -34,29 +34,29 @@ module Engine
           super
           return unless company.sym == 'M&C'
 
-          # Add a revenue bonus for the corporation
-          # that remains until corporation change president
-          ability = Engine::Ability::RouteBonus.new(
-              type: :route_bonus,
-              name: 'Specific route bonuses',
-              value: '$20 / $40',
-              **{
-                'bonuses' =>
-                  [
-                    {
-                      'route_name' => 'Robert E. Lee: Atlanta-Birmingham',
-                      'hexes' => %w[G4 G8],
-                      'bonus' => 20,
-                    },
-                    {
-                      'route_name' => 'Pan American: Nashville-Mobile',
-                      'hexes' => %w[A4 Q2],
-                      'bonus' => 40,
-                    },
-                  ],
-                }
-            )
-          @current_entity.add_ability(ability)
+          # # Add a revenue bonus for the corporation
+          # # that remains until corporation change president
+          # ability = Engine::Ability::RouteBonus.new(
+          #     type: :route_bonus,
+          #     name: 'Specific route bonuses',
+          #     value: '$20 / $40',
+          #     **{
+          #       'bonuses' =>
+          #         [
+          #           {
+          #             'route_name' => 'Robert E. Lee: Atlanta-Birmingham',
+          #             'hexes' => %w[G4 G8],
+          #             'bonus' => 20,
+          #           },
+          #           {
+          #             'route_name' => 'Pan American: Nashville-Mobile',
+          #             'hexes' => %w[A4 Q2],
+          #             'bonus' => 40,
+          #           },
+          #         ],
+          #       }
+          #   )
+          # @current_entity.add_ability(ability)
         end
       end
     end
