370e93864d3e6c3f6b695f481a8ea02012ca120a
perwestling <per.o.westling@gmail.com>
2020-07-27 13:57:45 +0200

    Correction of removal route_bonus ability when presidency changed

--- lib/engine/round/g_18_al/stock.rb
+++ lib/engine/round/g_18_al/stock.rb
@@ -8,36 +8,38 @@ module Engine
       class Stock < Stock
         def initialize(entities, game:, **opts)
           super
-          @bonus_controller = player_controlling_route_bonus_ability
-          @game = game
+          @controller_at_start = current_controller
         end
 
         def log_pass(entity)
-          begin
-            ability = route_bonus_ability
-            ability.owner.remove_ability(ability.type)
-            @bonus_controller = nil
-            @game.log << "Removed from #{ability.owner.name} due to presidency change: #{ability.name}"
-          end unless @bonus_controller.nil? || @bonus_controller == player_controlling_route_bonus_ability
-
           super
+          return if @controller_at_start.nil? || @controller_at_start == current_controller
+
+          @log << "#{route_bonus_ability.owner.name} removes route bonuses as presidency changed"
+          president_changed(route_bonus_ability.owner)
+          @controller_at_start = nil
         end
 
         private
 
-        def player_controlling_route_bonus_ability
-          ability = route_bonus_ability
-          ability.nil? ? nil : ability.owner.owner
+        def current_controller
+          route_bonus_ability&.owner&.owner
         end
 
         def route_bonus_ability
-          @corporations.each do |corporation|
+          @game.corporations.each do |corporation|
             corporation.abilities(:route_bonus) do |ability|
               return ability
             end
           end
 
           nil
+        end
+
+        def president_changed(corporation)
+          corporation.abilities(:route_bonus) do |ability|
+            corporation.remove_ability(ability)
+          end
         end
       end
     end
