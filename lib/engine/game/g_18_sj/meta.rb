7d45c3e59d829b46a8f446ea40b5f71ab45670f9
perwestling <per.o.westling@gmail.com>
2021-07-16 17:34:25 +0200

    [18SJ] Add special 2 player variant

--- lib/engine/game/g_18_sj/meta.rb
+++ lib/engine/game/g_18_sj/meta.rb
@@ -25,6 +25,12 @@ module Engine
             short_name: 'The Oscarian Era',
             desc: 'Full cap only, sell even if not floated, no par at 100',
           },
+          {
+            sym: :two_player_variant,
+            short_name: 'A.W. Edelswärds 2 Player Variant',
+            desc: 'A.W. Edelswärd "automa" plays the 3rd player',
+            players: [2],
+          },
         ].freeze
       end
     end
